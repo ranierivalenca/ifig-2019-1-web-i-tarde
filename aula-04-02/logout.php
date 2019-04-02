fc52a27b75d8678183a82e3317006e0c05ea024a
Ranieri <ranierivalenca@gmail.com>
2019-04-02 13:58:01 -0300

    Sair redireciona automaticamente para index

--- aula-04-02/logout.php
+++ aula-04-02/logout.php
@@ -4,16 +4,6 @@ include 'init.php';
 
 logout();
 
+header('location: index.php');
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <h1>Você está deslogado</h1>
-    <a href="index.php">Clique para voltar à tela inicial.</a>
-</body>
-</html>
