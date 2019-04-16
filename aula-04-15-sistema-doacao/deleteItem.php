dedfc30f21804c020fb62b370efe8f27c6b4d77a
Ranieri <ranierivalenca@gmail.com>
2019-04-16 14:18:42 -0300

    Hadouken no código de delete

--- aula-04-15-sistema-doacao/deleteItem.php
+++ aula-04-15-sistema-doacao/deleteItem.php
@@ -2,33 +2,57 @@
 
 include 'init.php';
 
-if (!is_logged()) {
-    redirect('index.php');
+// if (!is_logged()) {
+//     redirect('index.php');
+// }
+
+
+// $id = $_GET['id'] ?? false;
+// if ($id === false) {
+//     redirect('index.php');
+// }
+
+
+// $items = file(ITEMS_FILE);
+// $items = array_map(function($el) {
+//     list($email) = explode(SEPARATOR, $el);
+//     return $email;
+// }, $items);
+
+// if ($items[$id] != userEmail()) {
+//     redirect('index.php');
+// }
+
+// $items = file(ITEMS_FILE);
+// unset($items[$id]);
+// file_put_contents(
+//     ITEMS_FILE,
+//     join("", $items)
+// );
+
+if(is_logged()) {
+    $id = $_GET['id'] ?? false;
+    if ($id !== false) {
+        $items = file(ITEMS_FILE);
+        $items = array_map(function($el) {
+            list($email) = explode(SEPARATOR, $el);
+            return $email;
+        }, $items);
+
+        if ($items[$id] == userEmail()) {
+            $items = file(ITEMS_FILE);
+            unset($items[$id]);
+            file_put_contents(
+                ITEMS_FILE,
+                join("", $items)
+            );
+        }
+
+    }
 }
 
 
-$id = $_GET['id'] ?? false;
-if ($id === false) {
-    redirect('index.php');
-}
 
-
-$items = file(ITEMS_FILE);
-$items = array_map(function($el) {
-    list($email) = explode(SEPARATOR, $el);
-    return $email;
-}, $items);
-
-if ($items[$id] != userEmail()) {
-    redirect('index.php');
-}
-
-$items = file(ITEMS_FILE);
-unset($items[$id]);
-file_put_contents(
-    ITEMS_FILE,
-    join("", $items)
-);
 redirect('index.php');
 
 ?>
