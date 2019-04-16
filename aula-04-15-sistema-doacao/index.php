04458a838067308989c39782a75da3254e4c2079
Ranieri <ranierivalenca@gmail.com>
2019-04-16 13:44:06 -0300

    Adicionando dados do usuário na tabela de items

--- aula-04-15-sistema-doacao/index.php
+++ aula-04-15-sistema-doacao/index.php
@@ -3,6 +3,15 @@
 include 'init.php';
 
 $items = file(ITEMS_FILE);
+$users = file(USERS_FILE);
+// echo "<pre>";
+// print_r($items);
+// echo "</pre>";
+$usersEmails = [];
+foreach($users as $userRow) {
+    list($email, , $nome, $fone) = explode(SEPARATOR, $userRow);
+    $usersEmails[$email] = [$nome, $fone];
+}
 $items = array_map(function($el) {
     list($email, $name, $desc) = explode(SEPARATOR, $el);
     return [
@@ -36,7 +45,10 @@ $items = array_map(function($el) {
                 <td><?= $item['item'] ?></td>
                 <td><?= $item['desc'] ?></td>
                 <?php if (is_logged()): ?>
-                    <td><?= $item['email'] ?></td>
+                    <?php
+                        $user = $usersEmails[$item['email']];
+                    ?>
+                    <td><?= $user[0] ?> (<?= $user[1] ?>)</td>
                     <td>
 
                     </td>
